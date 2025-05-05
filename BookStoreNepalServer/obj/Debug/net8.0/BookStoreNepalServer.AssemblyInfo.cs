54d2f3fb45d6c63564099bc051fdeace982e2651
ankitchemjong <npp.rasik@gmail.com>
2025-05-06 01:45:36 +0545

    check

--- BookStoreNepalServer/obj/Debug/net8.0/BookStoreNepalServer.AssemblyInfo.cs
+++ BookStoreNepalServer/obj/Debug/net8.0/BookStoreNepalServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreNepalServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cacdc75aec8760428cf83b6e569650b82a3a3a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8930f10ff6ae4d57da74267973131d9c478bd264")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreNepalServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreNepalServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
